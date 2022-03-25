cb7ae9857172c6a2dd80da1e2839d56a1d3e07c0
Austin001316 <91763031+Austin001316@users.noreply.github.com>
2022-03-25 14:00:18 -0500

    Add files via upload

---
+++ How Web Works.js
@@ -0,0 +1,28 @@
+// Question 1
+// Hyper text transfer protocol, determines how information is sent and requested
+
+// Question 2
+// Uniform resource locator, an address to request info from servers.
+
+// Question 3
+// Domain name system, converts the url to an ip address.
+
+// Question 4
+// A querry string allows you to pass in terms to the server, usually a search term to locate something on the server
+
+// Question 5
+// GET, POST. Get is just a way to recieve information from the server. Post is a way to submit something to the server expecting a change to happen. "Get, no side effects.""Post, side effects intended"
+
+// Question 6
+// A request from a client to a server which follows the http protocol
+
+// Question 7
+// A response from a server to a client which follows the http protocol
+
+// Question 8
+// Headers provide additional content about the request or response. Request Headers: Accept-Language, Accept.
+//Response headers:  Content-Type, Last-Modified
+
+// Question 9
+// Your browser resolves the name into an ip address using DNS, then makes a request to the IP address including headers. Next the server sends a response. The browser then makes a DOM from that HTML and finds any other resources needed, like images and CSS.
+
